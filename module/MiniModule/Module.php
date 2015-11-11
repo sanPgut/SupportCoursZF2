1aa9ac0336848eb503a296c290ff36bbc39dd40c
trannois <harold.trannois@u-picardie.fr>
2015-11-11 21:33:50 +0100

    Formulaire maintenant de type Zend\Form\Form Entierement créé dans le controller Ce n'est pas très S.O.L.I.D. surtout pas S. & D.

--- module/MiniModule/Module.php
+++ module/MiniModule/Module.php
@@ -41,5 +41,4 @@ class Module implements ConfigProviderInterface, BootstrapListenerInterface, Aut
             ),
         );
     }
-
 }
--- module/MiniModule/src/MiniModule/Controller/IndexController.php
+++ module/MiniModule/src/MiniModule/Controller/IndexController.php
@@ -1,6 +1,7 @@
 <?php
 namespace MiniModule\Controller;
 
+use Zend\Form\Factory;
 use Zend\Mvc\Controller\AbstractActionController;
 
 /**
@@ -17,8 +18,39 @@ class IndexController extends AbstractActionController
 
     public function formAction()
     {
-        return array();
+        $configForm = array(
+            'elements' => array(
+                // la saisie du login (type text)
+                array(
+                    'spec' => array(
+                        'type' => 'Zend\Form\Element\Text',
+                        'name' => 'log',
+                        'attributes' => array(
+                            'size' => '20',
+                        ),
+                        'options' => array(
+                          'label' => 'Login : ',
+                        ),
+                    ),
+                ),
+                // le boutton de validation
+                array(
+                    'spec' => array(
+                        'type' => 'Zend\Form\Element\Submit',
+                        'name' => 'submit',
+                        'attributes' => array(
+                            'value' => 'Suite',
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        $factory = new Factory();
+        $form = $factory->createForm( $configForm );
+        return array( 'form' => $form );
     }
+
     public function traiteAction()
     {
         return array( 'login' => $_GET['log'] );
